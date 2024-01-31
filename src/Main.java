4e8b021591edebfbc749973a72c3bde2f8ee7f62
Alex <alexbatul@mail.ru>
2024-01-31 21:53:21 +0600

    change Main

--- src/Main.java
+++ src/Main.java
@@ -4,26 +4,17 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         List<Integer> intList = Arrays.asList(1, 2, 5, 16, -1, -2, 0, 32, 3, 5, 8, 23, 4);
-        int pos = 0;
-        for (int x : intList) {
-            if (x > 0) {
-                pos++;
-            }
+        List<Integer> posList = new ArrayList<>();
+
+        for (Integer n : intList) {
+            if (n > 0 && n % 2 == 0)
+                posList.add(n);
         }
-        int[] listpos = new int[pos];
-        int countpos = 0;
-        for (int x : intList) {
-            if (x > 0) {
-                listpos[countpos] = x;
-                countpos++;
-            }
-        }
-        for (int number : listpos) {
-            if (number % 2 == 0) {
-                System.out.println(number);
-            }
-            Arrays.sort(listpos);
-        }
+
+        posList.sort(Comparator.naturalOrder());
+
+        for (Integer n : posList)
+            System.out.println(n);
 
 
     }
